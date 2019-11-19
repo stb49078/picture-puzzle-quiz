f9e39932a912beb17634e8933ba32b32e2fce2a2
Stefan Bauer <stb@stefanbauer.at>
2019-11-19 17:46:36 +0100

    initial version

---
+++ src/main/java/at/stefanbauer/picturepuzzlequiz/MainMenu.java
@@ -0,0 +1,6 @@
+package at.stefanbauer.picturepuzzlequiz;
+
+import javafx.scene.control.MenuBar;
+
+public class MainMenu extends MenuBar {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module picture.puzzle.quiz {
+	requires javafx.graphics;
+	requires javafx.controls;
+
+	exports at.stefanbauer.picturepuzzlequiz to javafx.graphics;
+}
